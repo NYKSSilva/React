b9baf0fb98cace5418a813e13ab401af2b0fc9fb
NYKSSilva <nicollysantossilva0508@gmail.com>
2026-02-24 11:14:11 -0300

    começo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {Heading} from './component/heading.jsx'
+export function App() {
+
+  return (
+   <>
+   <h1>Hello</h1>
+   <div>
+    <Heading> Componente Heading</Heading>
+    </div>
+   </>
+  )
+}
+
+
